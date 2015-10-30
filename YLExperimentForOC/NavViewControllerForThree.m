9ae20476027d8895e624ef14f99629ce57285116
Mr_db <jxjgssylsg@126.com>
2015-10-30 19:52:27 +0800

    UITabbarController add some file

---
+++ YLExperimentForOC/NavViewControllerForThree.m
@@ -0,0 +1,21 @@
+//
+//  ViewController.m
+//  UIScreenEdgePanGestureRecognizer
+//
+//  Created by Jazys on 15/3/25.
+//  Copyright (c) 2015年 Jazys. All rights reserved.
+//
+
+#import "NavViewControllerForThree.h"
+
+@interface NavViewControllerForThree ()
+@end
+
+@implementation NavViewControllerForThree
+
+- (void)viewDidLoad {
+//    NSLog(@"view did load");
+//    [self.navigationController setNavigationBarHidden:YES animated:NO];
+}
+
+@end
