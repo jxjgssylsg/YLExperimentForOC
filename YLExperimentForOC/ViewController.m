114d90ed53d05bed054afd8f065c48246f1d0d19
jxjgssylsg <jxjgssylsg@126.com>
2016-04-26 18:30:26 +0800

    <增加代码>二维码初始版

--- YLExperimentForOC/ViewController.m
+++ YLExperimentForOC/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "QREncoding.h"
 @interface ViewController ()
 
 @end
@@ -16,9 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self creatQRCode];
 }
+- (void)creatQRCode
+{
+    CGFloat imageSize = ceilf(self.view.bounds.size.width * 0.6f);
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(floorf(self.view.bounds.size.width * 0.5f - imageSize * 0.5f), floorf(self.view.bounds.size.height * 0.5f - imageSize * 0.5f), imageSize, imageSize)];
+    //生成二维码图片
+    imageView.image = [QREncoding createQRcodeForString:@"http://www.baidu.com" withSize:imageView.bounds.size.width withRed:255 green:0 blue:0];
+    //添加中心图片
+    UIImageView *centerImage = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zhifubao.png"]];
+    centerImage.frame= CGRectMake(0, 0, 38.f, 38.f);
+    centerImage.center = CGPointMake(CGRectGetWidth(imageView.frame)/2.f, CGRectGetHeight(imageView.frame)/2.f);
+    [imageView addSubview:centerImage];
+    [self.view addSubview:imageView];
 
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
