ca21d4c1d66fc4ad34c11a208c883c331e6b263e
jxjgssylsg <jxjgssylsg@126.com>
2016-04-26 16:11:56 +0800

    Initial Commit

---
+++ YLExperimentForOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLExperimentForOC
+//
+//  Created by mingdffe on 16/4/26.
+//  Copyright © 2016年 yilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
