27c246dc0ce87350a09ab2eb47ed7589eeded8f8
Mr_db <jxjgssylsg@126.com>
2016-05-16 23:59:39 +0800

    median of two array use quick sort thought

--- YLExperimentForOC/Algorithm.m
+++ YLExperimentForOC/Algorithm.m
@@ -124,7 +124,38 @@
    
    [快速排序]
    [BFPRT算法] 分组的思想,比较精妙,也比较麻烦的感觉,利用的是不断分治找中位数,然后根据数学思想的过程.pass
-     
+           def median(A, B):
+           m, n = len(A), len(B)
+           if m > n:
+           A, B, m, n = B, A, n, m
+           if n == 0:
+           raise ValueError
+           
+           imin, imax, half_len = 0, m, (m + n + 1) / 2
+           while imin <= imax:
+           i = (imin + imax) / 2
+           j = half_len - i
+           if j > 0 and i < m and B[j-1] > A[i]:
+           # i is too small, must increase it
+           imin = i + 1
+           elif i > 0 and j < n and A[i-1] > B[j]:
+           # i is too big, must decrease it
+           imax = i - 1
+           else:
+           # i is perfect
+           if i == 0: max_of_left = B[j-1]
+           elif j == 0: max_of_left = A[i-1]
+           else: max_of_left = max(A[i-1], B[j-1])
+           
+           if (m + n) % 2 == 1:
+           return max_of_left
+           
+           if i == m: min_of_right = B[j]
+           elif j == n: min_of_right = A[i]
+           else: min_of_right = min(A[i], B[j])
+           
+           return (max_of_left + min_of_right) / 2.0
+
      int partition(int a[], int l, int r) //对数组a下标从l到r的元素进行划分
        {
        //随机选取一个数作为划分的基数
