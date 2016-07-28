0135eb26dd8c25422c4b2c4f15c96126faa0afb5
Mr_db <jxjgssylsg@126.com>
2016-07-29 07:44:47 +0800

    go to cp and resume

--- YLExperimentForOC/AnswerVCCellDataModelFrame.h
+++ YLExperimentForOC/AnswerVCCellDataModelFrame.h
@@ -9,14 +9,16 @@
 #import <Foundation/Foundation.h>
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "DataModel.h"
+#import "AnswerVCCellDataModel.h"
 
 @interface AnswerVCCellDataModelFrame : NSObject
 
-@property (assign, nonatomic) CGRect readLabelFrame;    // if unread, new label Frame
-@property (assign, nonatomic) CGRect avatarFrame;       // Avatar Frame
-@property (assign, nonatomic) CGRect contentFrame;      // content frame
-@property (assign, nonatomic) CGRect gapLineFrame;      // line frame
-@property (assign, nonatomic) CGRect timeFrame;         // time frame
+@property (assign, nonatomic) CGRect readLabelFrame;        // if unread, new label Frame
+@property (assign, nonatomic) CGRect avatarFrame;           // Avatar Frame
+@property (assign, nonatomic) CGRect contentFrame;          // content frame
+@property (assign, nonatomic) CGRect gapLineFrame;          // line frame
+@property (assign, nonatomic) CGRect timeFrame;             // time frame
+@property (strong, nonatomic) AnswerVCCellDataModel *model; // 
+@property (assign, nonatomic) CGFloat cellHeight;           // height of cell
 
 @end
--- YLExperimentForOC/AnswerVCCellDataModelFrame.m
+++ YLExperimentForOC/AnswerVCCellDataModelFrame.m
@@ -12,8 +12,76 @@
 
 @implementation AnswerVCCellDataModelFrame
 
-//+ (instancetype)DataModelFrameWithModel:(DataModel *)model {
-//    return [[DataModelFrame alloc] initWithModel:model];
-//}
++ (instancetype)DataModelFrameWithModel:(AnswerVCCellDataModel *)model {
+    return [[AnswerVCCellDataModelFrame alloc] initWithModel:model];
+}
 
++ (NSMutableArray *)DataModelFrameWithArray:(NSMutableArray *)arr {
+    NSMutableArray *data = [NSMutableArray array];
+    for (AnswerVCCellDataModel *model in arr) {
+        AnswerVCCellDataModelFrame *newM = [AnswerVCCellDataModelFrame DataModelFrameWithModel:model];
+        [data addObject:newM];
+    }
+    return data;
+}
+
+- (instancetype)initWithModel:(AnswerVCCellDataModel *)model {
+    if (self = [super init]) {
+        self.model = model;
+    }
+    return self;
+}
+
+
+- (CGFloat)cellHeight {
+    if (_cellHeight == 0) { // only calculate once
+        CGFloat margin = 10;
+        
+        // 头像
+        CGFloat iconX = margin;
+        CGFloat iconY = margin;
+        CGFloat iconWH = 30;
+        self.iconFrame = CGRectMake(iconX, iconY, iconWH, iconWH);
+        
+        // 昵称(姓名)
+        CGFloat nameY = iconY;
+        CGFloat nameX = CGRectGetMaxX(self.iconFrame) + margin;
+        // 计算文字所占据的尺寸
+        NSDictionary *nameAttrs = @{ NSFontAttributeName : [UIFont systemFontOfSize:17] };
+        CGSize nameSize = [self.model.name sizeWithAttributes:nameAttrs];
+        self.nameFrame = (CGRect){{nameX, nameY}, nameSize};
+        
+        // 会员图标
+        if (self.model.vip) {
+            CGFloat vipW = 14;
+            CGFloat vipH = nameSize.height;
+            CGFloat vipY = nameY;
+            CGFloat vipX = CGRectGetMaxX(self.nameFrame) + margin;
+            self.vipFrame = CGRectMake(vipX, vipY, vipW, vipH);
+        }
+        
+        // 文字
+        CGFloat textX = iconX;
+        CGFloat textY = CGRectGetMaxY(self.iconFrame) + margin;
+        CGFloat textW = [UIScreen mainScreen].bounds.size.width - 2 * textX;
+        CGSize textMaxSize = CGSizeMake(textW, MAXFLOAT);
+        NSDictionary *textAttrs = @{NSFontAttributeName : [UIFont systemFontOfSize:14]};
+        CGFloat textH = [self.model.text boundingRectWithSize:textMaxSize options:NSStringDrawingUsesLineFragmentOrigin attributes:textAttrs context:nil].size.height;
+        self.textFrame = CGRectMake(textX, textY, textW, textH);
+        
+        // 配图
+        if (self.model.picture) {
+            CGFloat pictureWH = 100;
+            CGFloat pictureX = textX;
+            CGFloat pictureY = CGRectGetMaxY(self.textFrame) + margin;
+            self.pictureFrame = CGRectMake(pictureX, pictureY, pictureWH, pictureWH);
+            
+            _cellHeight = CGRectGetMaxY(self.pictureFrame);
+        } else {
+            _cellHeight = CGRectGetMaxY(self.textFrame);
+        }
+        _cellHeight += margin;
+    }
+    return _cellHeight;
+}
 @end
