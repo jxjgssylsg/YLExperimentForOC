7f659a890bb81911fcfab83f31ddf55dd8e862a4
jxjgssylsg <jxjgssylsg@126.com>
2016-08-08 14:44:41 +0800

    code formatting

--- YLExperimentForOC/Macros.h
+++ YLExperimentForOC/Macros.h
@@ -12,6 +12,7 @@
 
 #define DefaultLinkColor @"#001cfe"
 
+#define ColorCreater(r, g, b, a) [UIColor colorWithRed:(r / 255.0) green:(g / 255.0) blue:(b / 255.0) alpha:a]
 #define COLOR(r,g,b) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:1]
 #define ColorWithAlpha(r,g,b,a) [UIColor colorWithRed:r/255.0 green:g/255.0 blue:b/255.0 alpha:a]
 
--- YLExperimentForOC/TableViewControllerTwo.m
+++ YLExperimentForOC/TableViewControllerTwo.m
@@ -8,8 +8,7 @@
 
 #import "TableViewControllerTwo.h"
 
-@interface TableViewControllerTwo ()<UITableViewDataSource, UITableViewDelegate>
-{
+@interface TableViewControllerTwo ()<UITableViewDataSource, UITableViewDelegate> {
     UITableView *_tableView;
     NSMutableArray *_cities;
     UIToolbar *_toolbar;
@@ -33,7 +32,7 @@
     [self addToolbar];
 }
 
--(void)initData {
+- (void)initData {
     NSMutableArray *GroupOne = [[NSMutableArray alloc] initWithArray: @[@"shandong",@"guangxi",@"taibei",@"上海",@"广州",@"天津",@"北京",@"shanghai"]];
     NSArray *GroupTwo =[[NSMutableArray alloc] initWithArray:  @[@"heibei",@"beijingdaxue",@"jiangxicaijing",@"上海",@"广州",@"天津",@"北京",@"shanghai",@"guangzhou",@"tianjing",@"南昌",@"xinjiang"]
                         ];
@@ -57,14 +56,14 @@
 // 每行是什么
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     // NSIndexPath是一个对象，记录了组和行信息
-    NSLog(@"生成单元格 (组：%li  行%li) ",(long)indexPath.section,(long)indexPath.row);
+    NSLog(@"生成单元格 (组：%li  行%li) ", (long)indexPath.section, (long)indexPath.row);
     NSString *text = nil;
     text = ((NSArray *)_cities[indexPath.section])[indexPath.row];
     
     // 重用cell
     static NSString *cellIdentifier = @"UITableViewCellIdentifierKey1";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier];
-    if(!cell){
+    if(!cell) {
         cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:cellIdentifier];// 注意UITableViewCellStyle 有多种,可点进去看看
     }
     if (0 == indexPath.section && 1 == indexPath.row) {
@@ -80,8 +79,7 @@
 }
 
 #pragma mark 返回每组头标题名称
-- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
-{
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
     if (0 == section) {
         return @"第一组";
     } else {
@@ -90,7 +88,7 @@
 }
 
 #pragma mark 返回每组尾部说明
--(NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section {
     if (0 == section) {
         return @"第一组尾部咯";
     } else {
@@ -101,23 +99,23 @@
 
 #pragma mark - 代理方法
 #pragma mark 设置分组头部标题内容高度
--(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
-    if(section==0){
+- (CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+    if(section == 0) {
         return 100;
     }
     return 30;
 }
 
 #pragma mark 设置尾部说明内容高度
--(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
-    if(section==0){
+- (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
+    if( section == 0) {
         return 100;
     }
     return 30;
 }
 
 #pragma mark 设置每行高度（每行高度可以不一样）
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     if( (0 == indexPath.row % 2) && (1 == indexPath.section) ) {
         return 70;
     } else {
@@ -127,26 +125,26 @@
 }
 
 #pragma mark 返回每组标题索引
--(NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView{
+- (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
     NSLog(@"生成组索引");
-    NSArray *indexs=@[@"1",@"2"];
+    NSArray *indexs = @[@"1",@"2"];
     return indexs;
 }
 
 // 点击事件,刷新
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"你点击了 %ld  %ld",(long)indexPath.section,(long)indexPath.row);
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSLog(@"你点击了 %ld  %ld", (long)indexPath.section, (long)indexPath.row);
     // 第一组整体刷新,第二组局部刷新
     if (0 == indexPath.section) {
         [_tableView reloadData];
     } else {
-        [_tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationLeft];
+        [_tableView reloadRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationLeft]; // $$ 局部刷新
     }
 }
 
 #pragma mark 切换开关转化事件
 - (void)switchValueChange:(UISwitch *)sw {
-    NSLog(@"section:%li,switch:%i",(long)sw.tag, sw.on);
+    NSLog(@"section:%li,switch:%i", (long)sw.tag, sw.on);
     // 跳到指的row or section
     [_tableView scrollToRowAtIndexPath:[NSIndexPath indexPathForRow:6 inSection:1]
                         atScrollPosition:UITableViewScrollPositionBottom animated:NO];
@@ -160,13 +158,13 @@
     UIBarButtonItem *removeButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemTrash target:self action:@selector(remove)];
     UIBarButtonItem *flexibleButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
     UIBarButtonItem *addButton = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector(add)];
-    NSArray *buttonArray = [NSArray arrayWithObjects:removeButton,flexibleButton,addButton, nil];
+    NSArray *buttonArray = [NSArray arrayWithObjects:removeButton, flexibleButton, addButton, nil];
     _toolbar.items = buttonArray;
 }
 
 #pragma mark 删除操作和插入
 // 实现了此方法向左滑动就会显示删除按钮
--(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     NSMutableArray *group = ((NSMutableArray *)_cities[indexPath.section]);
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // 删除某行 cell
@@ -174,19 +172,19 @@
         [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];
         
         // 如果当前组中没有数据则移除组刷新整个表格
-        if (group.count==0) {
+        if (group.count == 0) {
             [_cities removeObject:_cities[indexPath.section]];
             [_tableView reloadData];
         }
     } else if(editingStyle == UITableViewCellEditingStyleInsert) {
          NSString *city = @"jinggangshan xinchengqu";
         [group insertObject:city atIndex:indexPath.row];
-        [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom];//注意这里没有使用reladData刷新
+        [tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationBottom]; // 注意这里没有使用 reladData 刷新, 另这里不能用局部刷新 reloadRowsAtIndexPaths 方法来插入
     }
 }
 #pragma mark 排序 只需要在编辑状态下就可以排序
 // 只要实现这个方法在编辑状态右侧就有排序图标
--(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
     NSMutableArray *group = ((NSMutableArray *)_cities[sourceIndexPath.section]);
     NSString *city = group[sourceIndexPath.row];
     [group removeObjectAtIndex:sourceIndexPath.row];
@@ -202,20 +200,20 @@
 }
 
 #pragma mark 删除
--(void)remove {
+- (void)remove {
     _isInsert = false;
-    [_tableView setEditing:!_tableView.isEditing animated:true];//每次都取反
+    [_tableView setEditing:!_tableView.isEditing animated:true]; // 每次都取反
 }
 
 #pragma mark 添加
--(void)add {
+- (void)add {
     _isInsert = true;
     [_tableView setEditing:!_tableView.isEditing animated:true];
 }
 
 
 #pragma mark 取得当前操作状态，根据不同的状态左侧出现不同的操作按钮
--(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
     if (_isInsert) {
         return UITableViewCellEditingStyleInsert;
     }
